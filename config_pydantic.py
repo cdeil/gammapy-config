692f1ba45d6c8a8165334f145a50b472836c43a1
Christoph Deil <Deil.Christoph@gmail.com>
2019-11-20 18:51:03 +0100

    update

--- config_pydantic.py
+++ config_pydantic.py
@@ -1,6 +1,15 @@
 """Try Gammapy config with pydantic."""
+from typing import List
+import yaml
+from astropy.time import Time
 from astropy.coordinates import Angle
 from pydantic import BaseModel
+
+class PyDanticConfig:
+    validate_assignment = True
+    extra = "forbid"
+    # arbitrary_types_allowed = False
+
 
 class AngleType(Angle):
     @classmethod
@@ -11,22 +20,46 @@ class AngleType(Angle):
     def validate(cls, v):
         return Angle(v)
 
+class TimeType(Time):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if isinstance(v, Time):
+            return v
+        elif isinstance(v, str):
+            start, stop = v.split(" - ")
+            return Time([start, stop])
+        else:
+            raise ValueError()
+
 class LoggingConfig(BaseModel):
     level = "info"
     filename: str = None
 
+    Config = PyDanticConfig
+
 class GeneralConfig(BaseModel):
-    log: LoggingConfig = LoggingConfig()
+    log = LoggingConfig()
     outdir = "."
 
+    Config = PyDanticConfig
+
+
 class AnalysisConfig(BaseModel):
-    general: GeneralConfig
+    general = GeneralConfig()
 
     c1: int = 42
     c2: float = 99.9
     c3: str = "spam"
     a: AngleType = "99 deg"
+    time: TimeType = None
 
-    class Config:
-        validate_assignment = True
-        # arbitrary_types_allowed = True
+    obs_ids: List[int] = None
+
+    Config = PyDanticConfig
+
+    def to_yaml(self):
+        return yaml.dump(self.dict())
--- test_config_pydantic.py
+++ test_config_pydantic.py
@@ -1,22 +1,42 @@
+import pytest
+import yaml
 import astropy.units as u
-from gammapy.time import Time
+from astropy.time import Time
 from astropy.coordinates import Angle, SkyCoord
-from .config_pydantic import AnalysisConfig
+from config_pydantic import AnalysisConfig
 
+
+def test_config_basics():
+    config = AnalysisConfig()
+    config.a = "42 deg"
+    assert isinstance(config.a, Angle)
+
+    config.c1 = 1234
+
+    config.obs_ids = [41, 42]
+    assert config.obs_ids == [41, 42]
+
+    config.time = "2019-12-01 - 2020-03-01"
+    assert isinstance(config.time, Time)
+    assert str(config.time[0]) == "2019-12-01 00:00:00.000"
+
+def test_config_create_from_dict():
+    data = yaml.safe_load(open("example-config.yaml"))
+    # data = {"general": {"log": {"level": "warning"}}}
+    config = AnalysisConfig(**data)
+    assert config.general.log.level == "warning"
+
+def test_config_to_yaml():
+    config = AnalysisConfig()
+    assert "filename: null" in config.to_yaml()
+
+@pytest.mark.xfail(reason="TODO")
+def test_config_update_from_dict():
+    config = AnalysisConfig()
+    # TODO: implement this. pydantic offers it? Or copy code from Gammapy?
+    config.update_from_dict()
 
 if __name__ == '__main__':
-    c = AnalysisConfig()
-    print(c)
-    print(repr(c))
-    print(c.c1)
-    print(c.log)
-    print(c.log.level)
-
-    print(c.a)
-    c.a = "42 deg"
-    assert isinstance(c.a, Angle)
-
-    c.c1 = 1234
-    # c.hombre = "enrique"
-    # c.a = "99 deg"
-    print(c.a)
+    test_config_basics()
+    test_config_create_from_dict()
+    test_config_update_from_dict()
